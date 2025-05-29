e2aa7160750f8f2a9ed5d90a3fb07bd598d10b6f
joewaweruwanjema <wawerujoe@gmail.com>
2025-05-29 15:02:13 +0300

    add package-lock.json test/indexTest.js

--- test/indexTest.js
+++ test/indexTest.js
@@ -11,18 +11,31 @@ describe('basic math functions', function () {
   it("'add()' is a valid function", function() {
     expect(add).toExist
   })
+  function add( a, b ) {
+    return a + b;   
+  
+  }
 
   it("'subtract()' is a valid function", function() {
     expect(subtract).toExist
   })
+  function subtract( a, b ) {
+    return a - b;   
+  }
 
   it("'multiply()' is a valid function", function() {
     expect(multiply).toExist
   })
+  function multiply( a, b ) {
+    return a * b;   
+  }
 
   it("'divide()' is a valid function", function() {
     expect(divide).toExist
   })
+  function divide( a, b ) {np
+    return a / b;   
+  }
 
   it('add(a, b) adds two numbers and returns the result', function() {
     expect(add(a, b)).toEqual(a + b)
@@ -39,15 +52,24 @@ describe('basic math functions', function () {
   it('divide(a, b) divides a by b and returns the result', function() {
     expect(divide(a, b)).toEqual(a / b)
   })
+  function divide(a, b) {
+    return a / b;   
+  }
   
   it('increment(n) increments n and returns the result', function() {
     expect(increment(a)).toEqual(a + 1)
   })
+  function increment(n) {
+    return n + 1;   
+  }
   
   it('decrement(n) decrements n and returns the result', function() {
     expect(decrement(a)).toEqual(a - 1)
   })
 })
+function decrement(n) {
+  return n - 1;   
+}
 
 
 
@@ -56,6 +78,9 @@ describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
     expect(makeInt(a.toString())).toEqual(a)
   })
+  function makeInt(n) {
+    return parseInt(n, 10);   
+  }
 
   it('assumes base 10', function() {
     expect(makeInt('0x2328')).toEqual(0)
@@ -70,6 +95,9 @@ describe('preserveDecimal(n)', function() {
   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
     expect(preserveDecimal('2.222')).toBe(2.222)
   })
+  function preserveDecimal(n) {
+    return parseFloat(n);   
+  }
 
   it('returns NaN as appropriate', function() {
     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
